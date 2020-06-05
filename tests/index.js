7806772f0f215332f52e1c46b51712f39d38564d
Maurice Butler <maurice.butler@gmail.com>
2020-06-05 10:41:57 +1000

    add max age header and modernise

--- tests/index.js
+++ tests/index.js
@@ -1,80 +1,83 @@
-var test = require('grape'),
-    corsRoute = require('../');
+const test = require('tape');
+const corsRoute = require('..');
 
 test('corsRoute Exists', function (t) {
     t.plan(2);
 
     t.ok(corsRoute, 'corsRoute Exists');
-    t.equal(typeof corsRoute, 'function',  'corsRoute is an function');
+    t.equal(typeof corsRoute, 'function', 'corsRoute is an function');
 });
 
 test('corsRoute returns a function', function (t) {
     t.plan(2);
-    var result = corsRoute();
+    const result = corsRoute();
 
     t.ok(result, 'result Exists');
-    t.equal(typeof result, 'function',  'result is an function');
+    t.equal(typeof result, 'function', 'result is an function');
 });
 
 test('result sets headers', function (t) {
-    t.plan(9);
+    t.plan(11);
 
-    var testRequestHeader = 'foo',
-        headersAdded = 0,
-        validHeaders = ['Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
-        validValues = ['*', 'GET,PUT,POST,DELETE,OPTIONS', testRequestHeader],
-        testRequest = {
-            headers: {
-                'access-control-request-headers' : testRequestHeader
-            }
+    const testRequestHeader = 'foo';
+    let headersAdded = 0;
+    const validHeaders = [
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Methods',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Max-Age',
+    ];
+    const validValues = ['*', 'GET,PUT,POST,DELETE,OPTIONS', testRequestHeader, '86400'];
+    const testRequest = {
+        headers: {
+            'access-control-request-headers': testRequestHeader,
         },
-        testResponse = {
-            setHeader: function(header, value){
-                t.equal(header, validHeaders[headersAdded], '"' + header + '" header added.');
-                t.equal(value, validValues[headersAdded], '"' + value + '" value added.');
-                headersAdded++;
-            }
+    };
+    const testResponse = {
+        setHeader: function (header, value) {
+            t.equal(header, validHeaders[headersAdded], `"${header}" header added.`);
+            t.equal(value, validValues[headersAdded], `"${value}" value added.`);
+            headersAdded++;
         },
-
-
-    result = corsRoute(function(request, response){
+    };
+    const result = corsRoute(function (request, response) {
         t.equal(request, testRequest, 'recieved correct request');
         t.equal(response, testResponse, 'recieved correct response');
-        t.equal(headersAdded, 3, 'correct number of headers added');
+        t.equal(headersAdded, 4, 'correct number of headers added');
     });
 
     result(testRequest, testResponse);
 });
 
-
 test('result responds to OPTIONS request', function (t) {
-    t.plan(7);
+    t.plan(9);
 
-    var testRequestHeader = 'foo',
-        headersAdded = 0,
-        validHeaders = ['Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
-        validValues = ['*', 'GET,PUT,POST,DELETE,OPTIONS', testRequestHeader],
-        testRequest = {
-            method: 'OPTIONS',
-            headers: {
-                'access-control-request-headers' : testRequestHeader
-            }
+    const testRequestHeader = 'foo';
+    let headersAdded = 0;
+    const validHeaders = [
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Methods',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Max-Age',
+    ];
+    const validValues = ['*', 'GET,PUT,POST,DELETE,OPTIONS', testRequestHeader, '86400'];
+    const testRequest = {
+        method: 'OPTIONS',
+        headers: {
+            'access-control-request-headers': testRequestHeader,
         },
-        testResponse = {
-            setHeader: function(header, value){
-                t.equal(header, validHeaders[headersAdded], '"' + header + '" header added.');
-                t.equal(value, validValues[headersAdded], '"' + value + '" value added.');
-                headersAdded++;
-            },
-            end: function(){
-                t.equal(headersAdded, 3, 'correct number of headers added');
-            }
+    };
+    const testResponse = {
+        setHeader: function (header, value) {
+            t.equal(header, validHeaders[headersAdded], `"${header}" header added.`);
+            t.equal(value, validValues[headersAdded], `"${value}" value added.`);
+            headersAdded++;
         },
-
-
-    result = corsRoute(function(request, response){
-        t.fail('should have called response.end');
-    });
+        end: function () {
+            t.equal(headersAdded, 4, 'correct number of headers added');
+        },
+    };
+    const result = corsRoute(() => t.fail('should have called response.end'));
 
     result(testRequest, testResponse);
 });
